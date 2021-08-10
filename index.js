bac9c63f17fb5c9ae7a04c6b96a48bc8a3778d71
Dennis1-1995 <83753035+Dennis1-1995@users.noreply.github.com>
2021-08-10 16:45:28 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+function makePizza() {
+    console.log('Making the bottom');
+    console.log('Putting on the toppings');
+    console.log('Put the pizza in the oven');
+}
+
+function makeSushi(){
+    console.log('Making Sushi');
+}
+
+makeSushi();
+makeSushi();
+makeSushi();
+makePizza();
+makePizza();
